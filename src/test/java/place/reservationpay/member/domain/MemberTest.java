d2cc12bf83fd7298aba66a2a6caf83eafb44c22f
YouDongH <ydh312@naver.com>
2026-05-01 16:57:44 +0900

    test: 회원엔티티 테스트 작성 - 회원 엔티티 단위 테스트 작성

---
+++ src/test/java/place/reservationpay/member/domain/MemberTest.java
@@ -0,0 +1,29 @@
+package place.reservationpay.member.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import place.reservationpay.member.constant.Gender;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MemberTest {
+
+    @Test
+    @DisplayName("회원 생성시 등록일 및 등급은 일반회원으로 생성된다.")
+    void createMemberTest() {
+        // given
+        String loginId = "loginId";
+        String pw = "pw";
+        String birthday = "2002-02-25";
+        Gender gender = Gender.MAN;
+        String email = "email@naver.com";
+        String mobile = "010-6655-4433";
+        // when
+        Member result = Member.createMember(loginId, pw, birthday, gender, email, mobile);
+
+        // then
+        assertThat(result.getGrade()).isEqualTo("일반회원");
+        assertThat(result.getCreateAt()).isNotNull();
+
+    }
+}
