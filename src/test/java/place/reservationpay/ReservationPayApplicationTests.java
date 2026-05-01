9e10e77093aa62a3a77e28cfdfc8472bd8f9b094
YouDongH <ydh312@naver.com>
2026-05-01 13:55:38 +0900

    build: 초기 프로젝트 셋팅

---
+++ src/test/java/place/reservationpay/ReservationPayApplicationTests.java
@@ -0,0 +1,13 @@
+package place.reservationpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
